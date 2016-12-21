f7b27b58f6f7a51c13e83cd8af5a64462c68d033
harsha-chou <harsha-chou@users.noreply.github.com>
2016-12-21 16:26:42 +0530

    Update index.php
    making changes

--- index.php
+++ index.php
@@ -5,14 +5,18 @@
 		<style>
 			body {
        				 background-image: url(" https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ71_VRa1boTfWMrCgNUE25uZgJPvzGHuIftuLiiDR-Cci-k0_j");
-				} 
+				h3   {color: Red;}
+				h2   {color: Red;}
+				pre    {color: Red;}	
+			} 
 			<title>Main page</title>
 		</style>
 	</head>
 	<body>
-		<h3>Topics that are covered<h3><pre>
+		<h3>Topics that are covered<h3>
+			<pre>
 			1.<a href="Tools-list.html" >Tools-list</a>
 			2.<a href="learn.html" >Learning</a>
-</pre>
+			</pre>
 	</body>
 </html>
