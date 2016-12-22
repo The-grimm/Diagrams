353dbaafbdaa845e7b703144b23fd8ecc6e2feed
harsha-chou <harsha-chou@users.noreply.github.com>
2016-12-22 10:59:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,35 @@
 <html>
 	<head>
 		<style>
-			body 
-       				 background-image: url(" https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ71_VRa1boTfWMrCgNUE25uZgJPvzGHuIftuLiiDR-Cci-k0_j");
-				h3   {color: Red;}
-				h2   {color: Red;}
-				pre    {color: Red;}	
+.button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+				}
 			
+			body {
+				 background-image: url(" https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ71_VRa1boTfWMrCgNUE25uZgJPvzGHuIftuLiiDR-Cci-k0_j");
+				}
+				h3 {color: white;}
+					
 			<title>Main page</title>
 		</style>
 	</head>
 	<body>
 		<h3>Topics that are covered<h3>
-			<pre>
-			1.<a href="Tools-list.html" >Tools-list</a>
-			2.<a href="learn.html" >Learning</a>
-			</pre>
+		
+		<form action="Tools-list.html">	<input type="submit" class="button" value="Tools-list" />
+		</form>
+		<form action="learn.html"> <input type="submit" class="button" value="Learning" />
+		</form>	
+		
+		
 	</body>
 </html>
